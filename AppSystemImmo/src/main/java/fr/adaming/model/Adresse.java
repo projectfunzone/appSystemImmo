29d6af606997e64f23369e4e6cfe0d66713c592f
Adaming <Adaming@192.168.1.134>
2018-09-05 15:04:32 +0200

    Camille:classe model adresse

--- AppSystemImmo/src/main/java/fr/adaming/model/Adresse.java
+++ AppSystemImmo/src/main/java/fr/adaming/model/Adresse.java
@@ -1,5 +1,57 @@
 package fr.adaming.model;
 
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Adresse {
+
+	private String rue;
+	private String cp;
+	private String ville;
+	private String pays;
+
+	public Adresse() {
+		super();
+	}
+
+	public Adresse(String rue, String cp, String ville, String pays) {
+		super();
+		this.rue = rue;
+		this.cp = cp;
+		this.ville = ville;
+		this.pays = pays;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public String getCp() {
+		return cp;
+	}
+
+	public void setCp(String cp) {
+		this.cp = cp;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getPays() {
+		return pays;
+	}
+
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
 
 }
