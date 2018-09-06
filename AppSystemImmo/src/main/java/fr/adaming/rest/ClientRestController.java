83506d52aab38f382a55045bc0a155707714552a
Adaming <Adaming@192.168.1.164>
2018-09-06 15:38:38 +0200

    Thibault : ClientRest + ClientService pour Camille

--- AppSystemImmo/src/main/java/fr/adaming/rest/ClientRestController.java
+++ AppSystemImmo/src/main/java/fr/adaming/rest/ClientRestController.java
@@ -3,8 +3,11 @@ package fr.adaming.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.adaming.model.Client;
@@ -18,10 +21,57 @@ public class ClientRestController {
 	@Autowired
 	private IClientService clientService;
 
-	
+	/**
+	 * Recupérer la liste des Clients enregistrés
+	 * 
+	 * @return la liste des Clients
+	 */
 	@RequestMapping(value = "/liste", method = RequestMethod.GET, produces = "application/json")
 	public List<Client> getAllClients() {
 		return clientService.getAllClients();
+	}
+
+	/**
+	 * Recupérer le client par son id
+	 * 
+	 * @return le client correspondant à l'id
+	 */
+	@RequestMapping(value = "/getID", method = RequestMethod.GET, produces = "application/json")
+	public Client getClientbyID(@RequestParam("pID") int id) {
+		return clientService.getClient(id);
+	}
+
+	/**
+	 * Ajouter le client passé en paramètre
+	 * 
+	 * @return le client ajouté
+	 */
+	@RequestMapping(value = "/add", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	public Client addClient(@RequestBody Client cl) {
+		return clientService.addClient(cl);
+	}
+
+	/**
+	 * Modifier le client passé en paramètre
+	 * 
+	 * @return pas de retour
+	 */
+	@RequestMapping(value = "/update", method = RequestMethod.PUT, produces = "application/json", consumes = "application/json")
+	public void updateClient(@RequestBody Client cl) {
+		clientService.updateClient(cl);
+	}
+	
+	
+	/**
+	 * Supprimer le client ciblé
+	 * 
+	 * @return pas de retour
+	 */
+	@RequestMapping(value = "/del/{pId}", method = RequestMethod.DELETE)
+	public void deleteEtudiant(@PathVariable("pId") int id) {
+		Client clIn = new Client();
+		clIn.setId(id);
+		clientService.deleteClient(clIn);
 	}
 
 }
