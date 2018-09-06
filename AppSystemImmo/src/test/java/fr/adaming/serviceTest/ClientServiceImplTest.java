afed0bb60614d433bc9876e77538878061980e16
Adaming <Adaming@192.168.1.164>
2018-09-06 12:11:02 +0200

    Thibault : clienttest

---
+++ AppSystemImmo/src/test/java/fr/adaming/serviceTest/ClientServiceImplTest.java
@@ -0,0 +1,40 @@
+package fr.adaming.serviceTest;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.adaming.model.Client;
+import fr.adaming.service.IClientService;
+
+//permet de lance
+@RunWith(SpringJUnit4ClassRunner.class)
+// permet de définir la localisation du fichier de configuration
+// application-context pour lire les annotations
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/applicationContext.xml" })
+public class ClientServiceImplTest {
+
+	@Autowired
+	private IClientService clientService;
+	
+	//Cas de test de ajout Client
+		//@Ignore
+		@Test
+		@Transactional
+		@Rollback(false)
+		public void testAddClient() {
+			//int expectedAdd = 3;
+			Client clAdd=new Client("JOSSELIN", "Thibault", "0610444267", null, null);
+			//Client clIn = new Client(null, "toto", "titi", null, null, null, null, true);
+			clientService.addClient(clAdd);
+			assertEquals(1, clientService.getAllClients().size());
+		}
+}
