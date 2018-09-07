c7aaa5030c22b96f7e0bf1772a4f33c4433f7ecb
Adaming <Adaming@192.168.1.164>
2018-09-07 12:54:34 +0200

    Thibault : Client v1.0 (Controller/Service/Vue)

---
+++ AppSystemImmo/src/main/webapp/assets/js/controllers/clientController.js
@@ -0,0 +1,128 @@
+
+/*
+ * Controllers pour la gestion des Clients
+ */
+
+monApp.controller("findAllCtrl", function($scope) {
+	
+	// appel de la fonction Liste de 'clientProvider'
+	clientProvider.getListe(function(donnees) {
+		// stocker les données récupéré de service
+		$scope.liste = donnees;
+	})
+	
+	// appel de la fonction à partir du lien de la liste pour supprimer un client
+	
+	// initialiser l'etudiant de rootScope
+	
+	// appel de la fonction à partir du lien de la liste pour modifier un client
+	
+	
+}).controller("getCtrl", function($scope, clientProvider) {
+	
+	$scope.indice = false;
+	$scope.id = undefined;
+	$scope.msg = "";
+	$scope.search = function() {
+	
+	// appel de la fonction get de 'clientProvider' afin de récupérer le client
+		clientProvider.getById($scope.id, function(donnees) {
+
+			if (typeof donnees == 'object') {
+				$scope.client = donnees;
+				$scope.indice = true;
+			} else {
+				$scope.indice = false;
+				$scope.msg = "le client recherché n'existe pas"
+			}
+			;
+
+		})
+}
+}).controller("addCtrl", function($scope, clientProvider, $location) {
+
+	// initialiser l'objet dans le model du scope
+	$scope.clForm = {
+		id :"",
+		num : "",
+		nom :"",
+		prenom : "",
+		telPrive : "",
+		rue : "",
+		cp : "",
+		ville : "",
+		pays : ""
+	}
+		
+	// la fonction ajouter à partir du bouton d'appel "add"
+	$scope.add = function() {
+
+		// appel de la fonction service pour ajouter dans la bd
+		clientProvider.add($scope.clForm, function(donnees) {
+			if (typeof donnees == 'object') {
+				$scope.msg = "";
+				// redirection vers l'accueil
+				$location.path("liste");
+			} else {
+				$scope.msg = "L'enregistrement a échoué ! ";
+			}
+		})
+	}
+}).controller("updateCtrl",
+		function($scope, clientProvider, $location, $rootScope) {
+
+	// initialiser l'objet dans le model du scope s'il n'est pas dans le
+	// rootscope
+
+	if ($rootScope.clUpdate.id == undefined) {
+
+		$scope.clModif = {
+			id :undefined,
+			num : "",
+			nom :"",
+			prenom : "",
+			telPrive : "",
+			rue : "",
+			cp : "",
+			ville : "",
+			pays : ""
+		};
+	} else {
+		// si le rootScope est utilisé, alors on initialise clModif avec
+		$scope.clModif = $rootScope.clUpdate;
+	}
+
+	// la fonction update à partir du bouton d'appel "modifier"
+	$scope.update = function() {
+
+		// appel de la fonction service pour modifier dans la bd
+		clientProvider.update($scope.clModif, function(donnees) {
+			if (typeof donnees == 'object') {
+				$scope.msg = "";
+				// redirection vers l'accueil
+				$location.path("liste");
+			} else {
+				$scope.msg = "La modification a échoué ! ";
+			}
+		})
+	}
+
+}).controller("deleteCtrl",
+		function($scope, clientProvider, $location) {
+	$scope.id = undefined;
+
+	// la fonction appeler à partir du bouton
+	$scope.delet = function() {
+
+		// appel de la fonction service pour supprimer dans la bd
+		clientProvider.delet($scope.id, function(retour) {
+			if (retour == 'OK') {
+				$scope.msg = "";
+				// redirection vers l'accueil
+				$location.path("liste");
+			} else {
+				$scope.msg = "La suppression a échoué ! ";
+			}
+		})
+	}
+});
---
+++ AppSystemImmo/src/main/webapp/assets/js/services/clientService.js
@@ -0,0 +1,115 @@
+/*
+ * Service pour la gestion des Clients
+ * 
+ */
+
+monApp.factory("clientProvider", function($http) {
+
+	// récupérer la liste
+	function getListe(callBack) {
+		// récupérer la liste à partir du serveur
+		$http({
+			method : "GET",
+			url : "http://localhost:8080/AppSystemImmo/client/liste"
+		}).then(function successCallback(response) {
+			// stocker les données dans la callBack, afin de les
+			// transferer au
+			// controller, d'une manière asynchrone
+			callBack(response.data);
+		}, function errorCallback(response) {
+
+		});
+
+	}
+	
+	// récupérer un client avec son id
+	function get(id, callBack) {
+
+		// récupérer la liste à partir du serveur
+		$http({
+			method : "GET",
+			url : "http://localhost:8080/AppSystemImmo/client/get",
+			params : {
+				pId : id
+			}
+		}).then(
+		function successCallback(response) {
+			// stocker les données dans la callBack, afin de les transferer
+			// au controller, d'une manière asynchrone
+			callBack(response.data);
+		}, function errorCallback(response) {
+
+		});
+
+	}
+	
+	
+	//ajouter un client
+	function add(clIn, callBack) {
+		$http(
+				{
+					method : "POST",
+					url : "http://localhost:8080/AppSystemImmo/client/add",
+					//format d'envoi
+					data : angular.toJson(clIn),
+					//format de reception
+					headers : {
+						'Content-Type' : 'application/json'
+					}
+				}).then(function successCallback(response) {
+			// stocker les données dans la callBack, afin de les
+			// transferer au controller, d'une manière asynchrone
+			callBack(response.data);
+		}, function errorCallback(response) {
+
+		});
+	}
+	
+	// modifier un client dans la bd
+	function update(clIn, callBack) {
+		$http(
+				{
+					method : "PUT",
+					url : "http://localhost:8080/AppSystemImmo/client/update",
+					data : angular.toJson(clIn),
+					headers : {
+						'Content-Type' : 'application/json'
+					}
+				}).then(function successCallback(response) {
+			// stocker les données dans la callBack, afin de les
+			// transferer au
+			// controller, d'une manière asynchrone
+			callBack(response.data);
+		}, function errorCallback(response) {
+			callback(response.data)
+		});
+	}
+	
+	// supprimer un client
+	function delet(id, callBack) {
+		$http(
+				{
+					method : "DELETE",
+					url : "http://localhost:8080/AppSystemImmo/client/delete/"
+							+ id,
+				}).then(function successCallback(response) {
+			// stocker les données dans la callBack, afin de les
+			// transferer au controller, d'une manière asynchrone
+			callBack(response.statusText);
+		}, function errorCallback(response) {
+			callback(response.statusText)
+		});
+	}
+
+
+	
+	// les retour de factory
+	return {
+		getListe : getListes,
+		get : gets,
+		add : adds,
+		update : updates,
+		delet : delets
+	}
+	
+});
