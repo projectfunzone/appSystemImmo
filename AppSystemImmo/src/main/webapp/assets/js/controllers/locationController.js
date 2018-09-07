6279805b75501e128f6ff2b667ebe5cfb2ffb9a8
Adaming <Adaming@192.168.1.134>
2018-09-07 17:22:04 +0200

    Camille:début méthode update Location

--- AppSystemImmo/src/main/webapp/assets/js/controllers/locationController.js
+++ AppSystemImmo/src/main/webapp/assets/js/controllers/locationController.js
@@ -1,34 +1,34 @@
-monApp.controller("locationCtrlAdd", function($scope, locationProvider, $location) {
-	
-	
-	$scope.habitation=true;
-	$scope.commercial=false;
-	$scope.terrain=false;
-	//initialiser l'objet dans le model du scope
+monApp.controller("locationCtrlAdd", function($scope, locationProvider,
+		$location) {
+
+	$scope.habitation = true;
+	$scope.commercial = false;
+	$scope.terrain = false;
+	// initialiser l'objet dans le model du scope
 	$scope.locIn = {
-			categorie:"",
-			type:"",
-			surface:"",
-			noChambre:"",
-			statut:"",
-			dateSoumis:"",
-			adresse: {
-				rue:"",
-				cp:"",
-				ville:"",
-				pays:""
-			},
-			dateDispo:"",
-			revenueCadastre:"",
-			photo:"",
-			caution:"",
-			loyer:"",
-			charge:"",
-			bail:"",
-			garniture:""
+		categorie : "",
+		type : "",
+		surface : "",
+		noChambre : "",
+		statut : "",
+		dateSoumis : "",
+		adresse : {
+			rue : "",
+			cp : "",
+			ville : "",
+			pays : ""
+		},
+		dateDispo : "",
+		revenueCadastre : "",
+		photo : "",
+		caution : "",
+		loyer : "",
+		charge : "",
+		bail : "",
+		garniture : ""
 	}
-	
-	$scope.add=function () {
+
+	$scope.add = function() {
 		locationProvider.adds($scope.locIn, function(donnees) {
 			if (typeof donnees == 'object') {
 				$scope.msg = "";
@@ -39,32 +39,96 @@ monApp.controller("locationCtrlAdd", function($scope, locationProvider, $locatio
 			}
 		})
 	}
-	
-	
-	
-    $scope.locIn.categorie = "Male";
 
-    $scope.RadioChange = function (s) {
-        $scope.CategorieSelected = s;
-        
-        if (s == "Habitation") {
-        	$scope.habitation=true;
-        	$scope.commercial=false;
-        	$scope.terrain=false;
-        }
-        
-        if (s == "Commercial") {
-        	$scope.habitation=false;
-        	$scope.commercial=true;
-        	$scope.terrain=false;
-        }
-        
-        if (s == "Terrain") {
-        	$scope.habitation=false;
-        	$scope.commercial=false;
-        	$scope.terrain=true;
-        }
-    };
+	// méthode pour permettre de changer la vue lors de la selection de la
+	// catégorie : n'affiche que les types de la catégorie concerné
+	$scope.RadioChange = function(s) {
+		$scope.CategorieSelected = s;
+
+		if (s == "Habitation") {
+			$scope.habitation = true;
+			$scope.commercial = false;
+			$scope.terrain = false;
+		}
+
+		if (s == "Commercial") {
+			$scope.habitation = false;
+			$scope.commercial = true;
+			$scope.terrain = false;
+		}
+
+		if (s == "Terrain") {
+			$scope.habitation = false;
+			$scope.commercial = false;
+			$scope.terrain = true;
+		}
+	};
+
+		
+}).controller("locationCtrlUpdate", function($scope, locationProvider,
+		$location) {
+
+	$scope.habitation = true;
+	$scope.commercial = false;
+	$scope.terrain = false;
+	// initialiser l'objet dans le model du scope
+	$scope.locIn = {
+		categorie : "",
+		type : "",
+		surface : "",
+		noChambre : "",
+		statut : "",
+		dateSoumis : "",
+		adresse : {
+			rue : "",
+			cp : "",
+			ville : "",
+			pays : ""
+		},
+		dateDispo : "",
+		revenueCadastre : "",
+		photo : "",
+		caution : "",
+		loyer : "",
+		charge : "",
+		bail : "",
+		garniture : ""
+	}
 	
-	
+	$scope.add = function() {
+		locationProvider.updates($scope.locIn, function(donnees) {
+			if (typeof donnees == 'object') {
+				$scope.msg = "";
+				// redirection vers l'accueil
+				$location.path("accueil");
+			} else {
+				$scope.msg = "L'ajout a échoué ! ";
+			}
+		})
+	}
+
+	// méthode pour permettre de changer la vue lors de la selection de la
+	// catégorie : n'affiche que les types de la catégorie concerné
+	$scope.RadioChange = function(s) {
+		$scope.CategorieSelected = s;
+
+		if (s == "Habitation") {
+			$scope.habitation = true;
+			$scope.commercial = false;
+			$scope.terrain = false;
+		}
+
+		if (s == "Commercial") {
+			$scope.habitation = false;
+			$scope.commercial = true;
+			$scope.terrain = false;
+		}
+
+		if (s == "Terrain") {
+			$scope.habitation = false;
+			$scope.commercial = false;
+			$scope.terrain = true;
+		}
+	};
+
 })
--- AppSystemImmo/src/main/webapp/assets/js/services/locationService.js
+++ AppSystemImmo/src/main/webapp/assets/js/services/locationService.js
@@ -18,9 +18,26 @@ monApp.factory("locationProvider", function($http) {
 	}
 	
 	
+	function update(locIn, callBack) {
+		$http(
+				{
+					method: "PUT",
+					url: "http://localhost:8080/AppSystemImmo/location/update",
+					data:angular.toJson(locIn),
+					headers : {'Content-Type':'application/json'}
+				}).then(function successCallback(response) {
+					// stocker les données dans la callBack, afin de les transferer au
+					// controller, d'une manière asynchrone
+					callBack(response.data);
+				}, function errorCallback(response) {
+					console.log("-------erreur")
+				});
+	}
+	
 	
 	return {
-		adds:add
+		adds:add,
+		updates:update
 	}
 	
 	
