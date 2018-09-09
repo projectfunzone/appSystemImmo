92294e4cd54e660b4322d6bb09ce7efa68b6aff0
inti0489 <inti0489@192.168.1.57>
2018-09-09 15:39:21 +0200

    jd : commit CRUD proprio (quelques bugs delete et update) avec pages associees + startedPage (choix du portail, liens inactifs)

--- AppSystemImmo/src/main/webapp/assets/js/controllers/proprietaireController.js
+++ AppSystemImmo/src/main/webapp/assets/js/controllers/proprietaireController.js
@@ -1,45 +1,190 @@
 // création des controllers qui concernent la classe Propriétaire
-monApp.controller("proprietaireCtrlAdd", function($scope, proprietaireProvider, $location) {
+monApp
+		.controller(
+				"proprietaireCtrlAdd",
+				function($scope, proprietaireProvider, $location) {
 
-	// initialiser l'objet (le formulaire d'ajout) dans le model du scope
-	$scope.proprioForm = {
-		nom : "",
-		prenom : "",
-		adresse : {
-			rue : "",
-			cp : "",
-			ville : "",
-			pays : ""
-		},
-		telPrive : "",
-		telPro : ""
-	}
+					// initialiser l'objet (le formulaire d'ajout) dans le model
+					// du scope
+					$scope.proprioForm = {
+						nom : "",
+						prenom : "",
+						adresse : {
+							rue : "",
+							cp : "",
+							ville : "",
+							pays : ""
+						},
+						telPrive : "",
+						telPro : ""
+					}
 
-	// appel de la fonction ajouter lorsque l'on clique sur le bouton "ajouter"
-	// de la vue
-	$scope.add = function() {
+					// appel de la fonction ajouter lorsque l'on clique sur le
+					// bouton "ajouter"
+					// de la vue
+					$scope.add = function() {
 
-		// appel de la fonction service pour ajouter le propriétaire dans la bd
-		proprietaireProvider.addS($scope.proprioForm, function(donnees) {
-			if (typeof donnees == 'object') {
-				$scope.msg = "";
+						// appel de la fonction service pour ajouter le
+						// propriétaire dans la bd
+						proprietaireProvider
+								.addS(
+										$scope.proprioForm,
+										function(donnees) {
+											if (typeof donnees == 'object') {
+												$scope.msg = "";
 
-				// redirection vers la page d'accueil
-				$location.path("accueil");
+												// redirection vers la page
+												// d'accueil
+												$location.path("accueil");
 
-			} else {
-				$scope.msg="L'ajout du propriétaire a échoué, veuillez essayer de nouveau";
-			}
-		})
-	}
-})
+											} else {
+												$scope.msg = "L'ajout du propriétaire a échoué, veuillez essayer de nouveau";
+											}
+										})
+					}
+				})
 
-.controller("proprietaireCtrlFindAll", function($scope, proprietaireProvider, $location){
-		// appel de la fonction de proprietaireProvider qui permet de récupérer la liste
-		proprietaireProvider.getListeS(function(donnees){
-			
-			// stocker les informations récupérées par l'intermédiaire du proprietaireService
-			$scope.liste=donnees;
-			
-		});
-	})
+		.controller(
+				"proprietaireCtrlGet",
+				function($scope, proprietaireProvider) {
+					$scope.indice = false;
+					$scope.id = undefined;
+					$scope.msg = "";
+					$scope.get = function() {
+						// appel de la fonction du proprietaireProvider afin de
+						// récupérer le propriétaire
+						proprietaireProvider
+								.getS(
+										$scope.id,
+										function(donnees) {
+											if (typeof donnees == 'object') {
+												$scope.proprio = donnees;
+												$scope.indice = true;
+											} else {
+												$scope.indice = false;
+												$scope.msg = "Le propriétaire que vous recherchez n'est pas répertorié dans nos fichiers"
+											}
+										})
+					}
+				})
+
+		.controller(
+				"proprietaireCtrlFindAll",
+				function($scope, proprietaireProvider, $location) {
+					// appel de la fonction de proprietaireProvider qui permet
+					// de récupérer la liste
+					proprietaireProvider.getListeS(function(donnees) {
+
+						// stocker les informations récupérées par
+						// l'intermédiaire du proprietaireService
+						$scope.liste = donnees;
+
+					});
+
+					// fonction appelée à partir du lien supprimer de la liste
+					$scope.deleteLien = function(proprioIn) {
+						proprietaireProvider.delet(proprioIn.id, function(
+								retour) {
+							// mettre à jour la liste
+							proprietaireProvider.getListe(function(donnees) {
+								// stocker les données récupéré de service
+								$scope.liste = donnees;
+							})
+						})
+					}
+
+					// initialiser le propriétaire de rootscop
+					$rootScope.proprioUpdate = {
+						id : undefined,
+						nom : "",
+						prenom : "",
+						adresse : {
+							rue : "",
+							cp : "",
+							ville : "",
+							pays : ""
+						},
+						telPrive : "",
+						telPro : ""
+					};
+
+					// fonction appelée à partir du lien modifier de la liste
+					$scope.updateLien = function(proprioIn) {
+						$rootScope.proprioUpdate = proprioIn;
+
+						// aller dans la vue modifier
+						$location.path("update");
+
+					}
+				})
+
+		.controller(
+				"proprietaireCtrlUpdate",
+				function($scope, proprietaireProvider, $location, $rootScope) {
+					// initialiser l'objet dans le modèle du scope
+
+					if ($rootScope.proprioUpdate.id == undefined) {
+						$scope.proprioModif = {
+							id : undefined,
+							nom : "",
+							prenom : "",
+							adresse : {
+								rue : "",
+								cp : "",
+								ville : "",
+								pays : ""
+							},
+							telPrive : "",
+							telPro : ""
+						}
+					} else {
+
+						$scope.proprioModif = $scope.proprioUpdate;
+
+					}
+
+					// la fonction update appelée à partir du bouton
+					// modifier de la
+					// vue
+					$scope.update = function() {
+						// appel de la fonction service pour modifier dans la bd
+						proprietaireProvider
+								.updateS(
+										$scope.proprioModif,
+										function(donnees) {
+											$scope.msg = "";
+											if (typeof donnees == 'object') {
+												// rediriger vers accueil
+												$location.path("accueil");
+											} else {
+												$scope.msg = "Les informations n'ont pas pu être modifiées";
+											}
+
+										});
+					}
+
+				})
+
+		.controller(
+				"proprietaireCtrlDelete",
+				function($scope, proprietaireProvider, $location) {
+					$scope.id = undefined;
+
+					// la fonction rechercher appelée à partir du bouton
+					// supprimer de la
+					// vue
+					$scope.delet = function() {
+						// appel de la fonction service pour supprimer dans la
+						// bd
+						proprietaireProvider.deletS($scope.id,
+								function(retour) {
+									if (retour == 'OK') {
+										// rediriger vers accueil
+										$location.path("accueil");
+									} else {
+										$scope.msg = "La suppression a échoué";
+									}
+								})
+					}
+
+				});
--- AppSystemImmo/src/main/webapp/assets/js/services/proprietaireServices.js
+++ AppSystemImmo/src/main/webapp/assets/js/services/proprietaireServices.js
@@ -1,44 +1,101 @@
 // création du service concernant la classe Propriétaire
 
-monApp.factory("proprietaireProvider", function($http) {
+monApp
+		.factory(
+				"proprietaireProvider",
+				function($http) {
 
-	// récupérer la liste des propriétaires de la bd
-	function getListe(callBack) {
+					// récupérer la liste des propriétaires de la bd
+					function getListe(callBack) {
 
-		// récupérer la liste à partir du serveur
-		$http({
-			method : "GET",
-			url : "http://localhost:8080/AppSystemImmo/proprietaire/liste"
-		}).then(function successCallBack(reponse) {
-			callBack(reponse.data);
-		}, function errorCallBack(reponse) {
+						// récupérer la liste à partir du serveur
+						$http(
+								{
+									method : "GET",
+									url : "http://localhost:8080/AppSystemImmo/proprietaire/liste"
+								}).then(function successCallBack(reponse) {
+							callBack(reponse.data);
+						}, function errorCallBack(reponse) {
 
-		});
+						});
 
-	}
+					}
 
-	// ajouter un proprietaire dans la bd
-	function add(proprioIn, callBack) {
-		$http({
-			method : "POST",
-			url : "http://localhost:8080/AppSystemImmo/proprietaire/add",
-			data : angular.toJson(proprioIn),
-			headers : {
-				'Content-Type' : 'application/json'
-			}
-		}).then(function successCallBack(reponse) {
-			// stocker les information dans le callback pour les envoyer au
-			// controller de manière asynchrone
-			callBack(reponse.data);
-		}, function errorCallBack(reponse) {
+					// récupérer un propriétaire par son ID
+					function get(id, callBack) {
+						$http
+								.get(
+										"http://localhost:8080/AppSystemImmo/proprietaire/get",
+										{
+											params : {
+												pId : id
+											}
+										}).then(
+										function successCallback(response) {
+											callBack(response.data);
+										}, function errorCallback(response) {
 
-		});
-	}
+										});
+					}
 
-	// les différents retours de la fonction factory
-	return {
-		addS : add,
-		getListeS : getListe
-	}
+					// ajouter un proprietaire dans la bd
+					function add(proprioIn, callBack) {
+						$http(
+								{
+									method : "POST",
+									url : "http://localhost:8080/AppSystemImmo/proprietaire/add",
+									data : angular.toJson(proprioIn),
+									headers : {
+										'Content-Type' : 'application/json'
+									}
+								}).then(function successCallBack(reponse) {
+							// stocker les information dans le callback pour les
+							// envoyer au
+							// controller de manière asynchrone
+							callBack(reponse.data);
+						}, function errorCallBack(reponse) {
 
-});
+						});
+					}
+
+					// modifier un propriétaire dans la base de données
+					function update(proprioIn, callBack) {
+						$http(
+								{
+									method : "PUT",
+									url : "http://localhost:8080/AppSystemImmo/proprietaire/update",
+									data : angular.toJson(proprioIn),
+									headers : {
+										'Content-Type' : 'application/json'
+									}
+								}).then(function successCallback(response) {
+							callBack(response.data);
+						}, function errorCallback(response) {
+
+						});
+					}
+
+					// supprimer un étudiant de la bd
+					function delet(id, callBack) {
+						$http(
+								{
+									method : "DELETE",
+									url : "http://localhost:8080/AppSystemImmo/proprietaire/delete"
+											+ id
+								}).then(function successCallback(response) {
+							callBack(response.statusText);
+						}, function errorCallback(response) {
+
+						});
+					}
+
+					// les différents retours de la fonction factory
+					return {
+						addS : add,
+						getListeS : getListe,
+						getS : get,
+						updateS : update,
+						deletS : delet
+					}
+
+				});
