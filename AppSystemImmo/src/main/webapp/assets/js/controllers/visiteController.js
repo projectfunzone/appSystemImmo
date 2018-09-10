d14bec3ac429eca44a538b0e88fe71cf3cc007a0
adaming <adaming@192.168.1.147>
2018-09-10 15:52:39 +0200

    Amandine : modif controller Visite pour select add

--- AppSystemImmo/src/main/webapp/assets/js/controllers/visiteController.js
+++ AppSystemImmo/src/main/webapp/assets/js/controllers/visiteController.js
@@ -3,127 +3,116 @@
  */
 
 // Récuperer la liste
-monApp.controller("visiteCrltGetAll", 
-		function($scope, visiteProvider, $rootScope){
-	visiteProvider.getListes(function(donnees){
-		$scope.liste=donnees;
-	})
-	
-	
-	
-	// Faire le lien supprimer et modifier
-	$scope.deletelien=function(id){
-		visiteProvider.deletes(id, function(retour){
-			// mettre à jour la liste
-			visiteProvider.getListes(function(donnees){
-				// stocker les données recup de service (collback)
-				$scope.liste=donnees;
+monApp.controller("visiteCrltGetAll",
+		function($scope, visiteProvider, $rootScope) {
+			visiteProvider.getListes(function(donnees) {
+				$scope.liste = donnees;
 			})
-		})	
-	}
-	
-	// initialiser la visite de root scope pour lien modifier visite
-	$rootScope.vUpdate={
-			id:undefined,
-			date:"",	
-	}
-	// fonction appelé à partir du lien de la liste pour modifier
-	$scope.updateLien=function(vIn){
-		$rootScope.vUpdate=vIn
-	}
-	
-	// aller dans la vue modif
-	// $location.path("visite/Update");
-	
-})
-	
 
-	
-	
+			// Faire le lien supprimer et modifier
+			$scope.deletelien = function(id) {
+				visiteProvider.deletes(id, function(retour) {
+					// mettre à jour la liste
+					visiteProvider.getListes(function(donnees) {
+						// stocker les données recup de service (collback)
+						$scope.liste = donnees;
+					})
+				})
+			}
+
+			// initialiser la visite de root scope pour lien modifier visite
+			$rootScope.vUpdate = {
+				id : undefined,
+				date : "",
+			}
+			// fonction appelé à partir du lien de la liste pour modifier
+			$scope.updateLien = function(vIn) {
+				$rootScope.vUpdate = vIn
+			}
+
+			// aller dans la vue modif
+			// $location.path("visite/Update");
+
+		})
+
 // Fonction ajouter une visite
-.controller("visiteCtrlAdd", function($scope, visiteProvider, $location, $rootScope){
-		
-	
-		visiteProvider.getListes(function(donnees){
-			$scope.liste=donnees;
-			console.log("-------liste")
-		})
-	
-	
-		
-	
-	
-	$scope.visiteForm={
-				date:"",
-				client_id:"", 
-				achat_id:"",
-				conseiller_id:""
-				
-		};
-		
-		// foctionnaliter du bouton ajouter de la vue
-		$scope.adds=function(){
-			visiteProvider.adds($scope.visiteForm, function(donnees){
-				if(typeof donnees=='object'){
-					$scope.msg="";
-					// rediriger vers la page liste
-					$location.path("visite/liste");
-				}else{
-					$scope.msg="L'ajout de votre visite a échoué!"
-				}
+.controller("visiteCtrlAdd",
+		function($scope, visiteProvider, $location, $rootScope) {
+
+			visiteProvider.getListes(function(donnees) {
+				$scope.liste = donnees;
+				console.log("-------liste")
 			})
-			
-		}
-		
-	
-})
 
+			$scope.visiteForm = {
+				date : "",
+				client : {
+					id : undefined
+				},
+				achat : {
+					id : undefined
+				},
+				conseiller : {
+					id : undefined
+				}
+			};
 
-.controller("visiteCtrlUpdate", function($scope, visiteProvider, $location, $rootScope) {
-	if($rootScope.vUpdate.id==undefined){
+			// foctionnaliter du bouton ajouter de la vue
+			$scope.adds = function() {
+				visiteProvider.adds($scope.visiteForm, function(donnees) {
+					if (typeof donnees == 'object') {
+						$scope.msg = "";
+						// rediriger vers la page liste
+						$location.path("visite/liste");
+					} else {
+						$scope.msg = "L'ajout de votre visite a échoué!"
+					}
+				})
 
-	$scope.vModif={
-			date:""
-		
-	}
-	}else{
-		$scope.vModif=$rootScope.vUpdate;
-	}
-	
-	// fonction du bouton modifier de la vue
-	$scope.updates=function(){
-		visiteProvider.updates($scope.vModif, function(donnees){
-			if(typeof donnees=='object'){
-				$scope.msg="";
-				$location.path("visite/liste");
-			}else{
-				$scope.msg="La modification a échoué!"
 			}
+
 		})
-	}
-	
-	
 
-	
-}).controller("visiteCtrlDelete",
-		function($scope, visiteProvider, $location) {
-	$scope.id = undefined;
+.controller("visiteCtrlUpdate",
+		function($scope, visiteProvider, $location, $rootScope) {
+			if ($rootScope.vUpdate.id == undefined) {
 
-	// la fonction appeler à partir du bouton
-	$scope.delet = function() {
+				$scope.vModif = {
+					date : ""
 
-		// appel de la fonction service pour supprimer dans la bd
-		visiteProvider.deletes($scope.id, function(retour) {
-			if (retour == 'OK') {
-				$scope.msg = "";
-				// redirection vers l'accueil
-				$location.path("visite/liste");
+				}
 			} else {
-				$scope.msg = "La suppression a échoué ! ";
+				$scope.vModif = $rootScope.vUpdate;
 			}
-		})
-	}
-});
 
+			// fonction du bouton modifier de la vue
+			$scope.updates = function() {
+				visiteProvider.updates($scope.vModif, function(donnees) {
+					if (typeof donnees == 'object') {
+						$scope.msg = "";
+						$location.path("visite/liste");
+					} else {
+						$scope.msg = "La modification a échoué!"
+					}
+				})
+			}
 
+		}).controller("visiteCtrlDelete",
+		function($scope, visiteProvider, $location) {
+			$scope.id = undefined;
 
+			// la fonction appeler à partir du bouton
+			$scope.delet = function() {
+
+				// appel de la fonction service pour supprimer dans la bd
+				visiteProvider.deletes($scope.id, function(retour) {
+					if (retour == 'OK') {
+						$scope.msg = "";
+						// redirection vers l'accueil
+						$location.path("visite/liste");
+					} else {
+						$scope.msg = "La suppression a échoué ! ";
+					}
+				})
+			}
+		});
