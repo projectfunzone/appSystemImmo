34dcbd4c82e7a78fc2936cbe078f4d544861c462
Adaming <Adaming@192.168.1.153>
2018-09-11 12:50:10 +0200

    ewen: correction bug carousel

--- AppSystemImmo/src/main/webapp/assets/js/controllers/conseillerController.js
+++ AppSystemImmo/src/main/webapp/assets/js/controllers/conseillerController.js
@@ -3,16 +3,22 @@
  */
 
 monApp.controller("conseillerCtrlFindAll",
-		function($scope, conseillerProvider, $location) {
+		function($scope, conseillerProvider, $rootScope, $location) {
 			conseillerProvider.getListe(function(donnees) {
 				$scope.liste = donnees;
-
+				
+				// initialiser le conseiller de rootScope
+				$rootScope.coUpdate = {
+					id : undefined,
+					nom : "",
+					mdp : "",
+				};
 			})
 		}).controller("conseillerCtrlAdd",
 		function($scope, conseillerProvider, $location) {
 
 			// initialiser l'objet dans le model du scope
-			$scope.clForm = {
+			$scope.coForm = {
 				id : "",
 				nom : "",
 				mdp : ""
@@ -31,5 +37,75 @@ monApp.controller("conseillerCtrlFindAll",
 					}
 				})
 			}
-		})
-			
+		}).controller("conseillerCtrlGet",
+		function($scope, conseillerProvider) {
+
+			$scope.indice = false;
+			$scope.id = undefined;
+			$scope.msg = "";
+			$scope.search = function() {
+
+				// appel de la fonction get de 'conseillerProvider' afin de
+				// récupérer le
+				// conseiller
+				conseillerProvider.get($scope.id, function(donnees) {
+
+					if (typeof donnees == 'object') {
+						$scope.co = donnees;
+						$scope.indice = true;
+					} else {
+						$scope.indice = false;
+						$scope.msg = "le conseiller recherché n'existe pas"
+					}
+					;
+
+				})
+			}
+		}).controller("conseillerCtrlUpdate",
+		function($scope, conseillerProvider, $location, $rootScope) {
+
+			// initialiser l'objet dans le model du scope s'il n'est pas dans le
+			// rootscope
+
+			if ($rootScope.coUpdate.id == undefined) {
+
+				$scope.coModif = {
+					id : undefined,
+					nom : "",
+					mdp : ""
+				};
+			} else {
+				// si le rootScope est utilisé, alors on initialise coModif avec
+				$scope.coModif = $rootScope.coUpdate;
+			}
+
+			// la fonction update à partir du bouton d'appel "modifier"
+			$scope.update = function() {
+
+				// appel de la fonction service pour modifier dans la bd
+				conseillerProvider.update($scope.coModif, function(retour) {
+					$scope.msg = "";
+					// redirection vers l'accueil
+					$location.path("liste");
+				})
+			}
+
+		}).controller("conseillerCtrlDelete",
+		function($scope, conseillerProvider, $location) {
+			$scope.id = undefined;
+
+			// la fonction appeler à partir du bouton
+			$scope.delet = function() {
+
+				// appel de la fonction service pour supprimer dans la bd
+				conseillerProvider.delet($scope.id, function(retour) {
+					if (retour == 'OK') {
+						$scope.msg = "";
+						// redirection vers l'accueil
+						$location.path("liste");
+					} else {
+						$scope.msg = "La suppression a échoué ! ";
+					}
+				})
+			}
+		});
--- AppSystemImmo/src/main/webapp/assets/js/services/conseillerServices.js
+++ AppSystemImmo/src/main/webapp/assets/js/services/conseillerServices.js
@@ -19,7 +19,7 @@ monApp.factory("conseillerProvider", function($http) {
 		});
 
 	}
-	
+
 	// ajouter un conseiller
 	function adds(coIn, callBack) {
 		$http({
@@ -39,10 +39,66 @@ monApp.factory("conseillerProvider", function($http) {
 
 		});
 	}
+
+	// récupérer un conseiller avec son id
+	function gets(id, callBack) {
+
+		// récupérer la liste à partir du serveur
+		$http({
+			method : "GET",
+			url : "http://localhost:8080/AppSystemImmo/conseiller/get",
+			params : {
+				pId : id
+			}
+		}).then(function successCallback(response) {
+			// stocker les données dans la callBack, afin de les transferer
+			// au controller, d'une manière asynchrone
+			callBack(response.data);
+		}, function errorCallback(response) {
+
+		});
+
+	}
+
+	// modifier un conseiller dans la bd
+	function updates(coIn, callBack) {
+		$http({
+			method : "PUT",
+			url : "http://localhost:8080/AppSystemImmo/conseiller/update",
+			data : angular.toJson(coIn),
+			headers : {
+				'Content-Type' : 'application/json'
+			}
+		}).then(function successCallback(response) {
+			// stocker les données dans la callBack, afin de les
+			// transferer au controller, d'une manière asynchrone
+			callBack(response.data);
+		}, function errorCallback(response) {
+			callback(response.data)
+		});
+	}
+
+	// supprimer un conseiller
+	function delets(id, callBack) {
+		$http({
+			method : "DELETE",
+			url : "http://localhost:8080/AppSystemImmo/conseiller/delete/" + id,
+		}).then(function successCallback(response) {
+			// stocker les données dans la callBack, afin de les
+			// transferer au controller, d'une manière asynchrone
+			callBack(response.statusText);
+		}, function errorCallback(response) {
+			callback(response.statusText)
+		});
+	}
+
 	// les retour de factory
 	return {
 		getListe : getListes,
+		get : gets,
 		add : adds,
-	}	
-	
+		update : updates,
+		delet : delets
+	}
+
 })
